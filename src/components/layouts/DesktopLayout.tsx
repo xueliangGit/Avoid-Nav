d13df903b68511d7aa03e6d6644c2b79d912f0ec
xuxueliang <xusloth@gmail.com>
2026-05-29 15:59:43 +0800

    feat(layout): DesktopLayout

---
+++ src/components/layouts/DesktopLayout.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import type { ReactNode } from 'react';
+
+interface Props {
+  controlPanel: ReactNode;
+  debugPanel: ReactNode;
+  mapElement: ReactNode;
+}
+
+export default function DesktopLayout({ controlPanel, debugPanel, mapElement }: Props) {
+  return (
+    <div className="relative w-full h-full">
+      <div className="absolute top-4 left-4 z-[2000] w-96 max-h-[calc(100vh-32px)] pointer-events-auto">
+        {controlPanel}
+      </div>
+      <div className="absolute top-4 right-4 z-[2000] w-80 h-[400px] pointer-events-auto">
+        {debugPanel}
+      </div>
+      {mapElement}
+    </div>
+  );
+}
